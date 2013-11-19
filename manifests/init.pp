8dbeeb259b82717687cc04735ef4a48c079bb84b
Toshiaki Koshiba <koshiba@qg8.so-net.ne.jp>
2013-11-19 10:14:21 +0900

    fix error in manifests/init.pp
    - ERROR: single quoted string containing a variable found on line 4
    - ERROR: trailing whitespace found on line 4

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class kobito ($version = '1.5.0') {
   package { 'Kobito':
     provider => 'compressed_app',
-    source   => 'http://kobito.qiita.com/download/Kobito_v${version}.zip'  
+    source   => "http://kobito.qiita.com/download/Kobito_v${version}.zip"
   }
 }
