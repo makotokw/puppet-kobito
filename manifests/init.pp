3a246654add5965eef1105017e517631fb5057db
Toshiaki Koshiba <koshiba@qg8.so-net.ne.jp>
2013-11-19 10:19:17 +0900

    fix warning in manifests/init.pp
    - WARNING: class not documented on line 1

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,10 @@
-class kobito ($version = '1.5.0') {
+# Public: Install Kobito(http://kobito.qiita.com/) for use with Boxen.
+#
+# Examples
+#
+#   include kobito
+
+class kobito ($version = '1.8.3') {
   package { 'Kobito':
     provider => 'compressed_app',
     source   => "http://kobito.qiita.com/download/Kobito_v${version}.zip"
