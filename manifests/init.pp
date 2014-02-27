f8391624fa2a371df7cdd0392bd91fe0b7359bd8
makoto_kw <makoto.kw@gmail.com>
2014-02-27 23:00:57 +0900

    KobitoApp Version 1.8.10

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #   include kobito
 
-class kobito ($version = '1.8.9') {
+class kobito ($version = '1.8.10') {
   package { 'Kobito':
     provider => 'compressed_app',
     source   => "http://kobito.qiita.com/download/Kobito_v${version}.zip"
